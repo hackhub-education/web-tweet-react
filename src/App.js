5f217b8b0b7551157d437b1481211eefc5d5bc77
Yan Hong <yanhong@WebDxDs-MacBook-Pro.local>
2018-06-10 08:28:15 -0700

    pass logo and avatar as props to Nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import Nav from './component/Nav';
+
 import logo from './img/logo.png';
 import avatar from './img/sample-avatar.png';
 
@@ -6,39 +9,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className="nav-bar">
-          <div className="container nav-container">
-            <ul>
-                <li><a href="index.html"><img className="logo" src={logo} alt="webdxd"/></a></li>
-                <li><a href="index.html">Home</a></li>
-            </ul>
-            <div>
-                <a href="profile.html"><img className="avatar-sm" src={avatar} alt="avatar"/></a>
-                {/* Remember to close image tag */}
-            </div> 
-        </div>
-        </nav>
+        <Nav logo={logo} avatar={avatar}></Nav>
         <div className="container">
-        <div className="col-2of5 bg-white profile">
+          <div className="col-2of5 bg-white profile">
             <div className="profile-content"></div>
-        </div>
-        <div className="col-3of5 bg-white">
+          </div>
+          <div className="col-3of5 bg-white">
             <div className="tweet">
-                <form id="tweet-form">
-                    <div className="row">
-                        <img className="avatar-sm v-top" src={avatar} alt="avatar"/>                       
-                        <textarea className="input-tweet" placeholder="What's up?" id="tweet-content"></textarea>
-                    </div>
-                    <div className="row tweet-actions">
-                        {/* <input type="hidden" role="uploadcare-uploader" name="content" id="tweet-image" data-public-key="7d92f12ba9b3c1d2afd1" data-images-only /> */}
-                        <button className="btn-clear" type="button"><i className="far fa-image" id="tweet-image-btn"></i></button>
-                        <button className="btn-primary" type="button" id="post-btn" disabled>Post</button>
-                    </div>
-                </form>
+              <form id="tweet-form">
+                <div className="row">
+                  <img className="avatar-sm v-top" src={avatar} alt="avatar" />
+                  <textarea className="input-tweet" placeholder="What's up?" id="tweet-content"></textarea>
+                </div>
+                <div className="row tweet-actions">
+                  {/* <input type="hidden" role="uploadcare-uploader" name="content" id="tweet-image" data-public-key="7d92f12ba9b3c1d2afd1" data-images-only /> */}
+                  <button className="btn-clear" type="button"><i className="far fa-image" id="tweet-image-btn"></i></button>
+                  <button className="btn-primary" type="button" id="post-btn" disabled>Post</button>
+                </div>
+              </form>
             </div>
             <div id="tweet-list"></div>
+          </div>
         </div>
-    </div>
       </div>
     );
   }
---
+++ src/component/Nav.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    render() {
+        return (
+            <nav className="nav-bar">
+                <div className="container nav-container">
+                    <ul>
+                        <li><a href="index.html"><img className="logo" src={this.props.logo} alt="webdxd" /></a></li>
+                        <li><a href="index.html">Home</a></li>
+                    </ul>
+                    <div>
+                        <a href="profile.html"><img className="avatar-sm" src={this.props.avatar} alt="avatar" /></a>
+                        {/* Remember to close image tag */}
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Nav;
