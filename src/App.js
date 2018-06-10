2cb9462cbc08da62a5b225b84723e7ed68097bf0
Yan Hong <yanhong@WebDxDs-MacBook-Pro.local>
2018-06-10 08:20:57 -0700

    basic structure of homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
+import logo from './img/logo.png';
+import avatar from './img/sample-avatar.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to WebDxD</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav className="nav-bar">
+          <div className="container nav-container">
+            <ul>
+                <li><a href="index.html"><img className="logo" src={logo} alt="webdxd"/></a></li>
+                <li><a href="index.html">Home</a></li>
+            </ul>
+            <div>
+                <a href="profile.html"><img className="avatar-sm" src={avatar} alt="avatar"/></a>
+                {/* Remember to close image tag */}
+            </div> 
+        </div>
+        </nav>
+        <div className="container">
+        <div className="col-2of5 bg-white profile">
+            <div className="profile-content"></div>
+        </div>
+        <div className="col-3of5 bg-white">
+            <div className="tweet">
+                <form id="tweet-form">
+                    <div className="row">
+                        <img className="avatar-sm v-top" src={avatar} alt="avatar"/>                       
+                        <textarea className="input-tweet" placeholder="What's up?" id="tweet-content"></textarea>
+                    </div>
+                    <div className="row tweet-actions">
+                        {/* <input type="hidden" role="uploadcare-uploader" name="content" id="tweet-image" data-public-key="7d92f12ba9b3c1d2afd1" data-images-only /> */}
+                        <button className="btn-clear" type="button"><i className="far fa-image" id="tweet-image-btn"></i></button>
+                        <button className="btn-primary" type="button" id="post-btn" disabled>Post</button>
+                    </div>
+                </form>
+            </div>
+            <div id="tweet-list"></div>
+        </div>
+    </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/normalize.css';
+import './css/fontawesome-all.css';
+import './css/columns.css';
+import './css/main.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
