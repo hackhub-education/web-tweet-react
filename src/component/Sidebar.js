6884249c8967aa825e87858f92624c2876c90022
Yan Hong <yan@hackhub.com>
2018-06-15 15:36:37 -0700

    edit profile

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -13,14 +13,23 @@ class SideBar extends Component {
     }
 
     handleIsAuth(Component) {
-        return this.props.token ? <Redirect to="/" /> : Component
+        if (this.props.token) {
+            if (this.props.profile.location && this.props.profile.bio) {
+                return <Redirect to="/" />
+            } else {
+                return <Redirect to="/profile/edit" />
+            }
+
+        } else {
+            return Component
+        }
     }
 
     render() {
         return (
             <div>
                 <Route path='(/|/profile)' exact render={() => (this.handleAuth(<Profile profile={this.props.profile} handleLogout={this.props.handleLogout} />))} />
-                <Route path='/profile/edit' render={() => (this.handleAuth(<ProfileForm profile={this.props.profile} />))} />
+                <Route path='/profile/edit' render={() => (this.handleAuth(<ProfileForm handleUserUpdate={this.props.handleUserUpdate} profile={this.props.profile} token={this.props.token}/>))} />
                 <Route path='/login' render={() => (this.handleIsAuth(<LoginForm handleUserUpdate={this.props.handleUserUpdate} />))} />
                 <Route path='/signup' render={() => (this.handleIsAuth(<SignupForm handleUserUpdate={this.props.handleUserUpdate} token={this.props.token} />))} />
             </div>
--- src/component/profile/ProfileForm.js
+++ src/component/profile/ProfileForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom'
 import axios from 'axios';
 import { baseUrl } from '../../config'
 
@@ -7,25 +8,62 @@ class ProfileForm extends Component {
         super(props)
         this.state = {
             avatarUrl: props.profile.avatarUrl,
-            location: props.profile.location,
-            bio: props.profile.bio,
-            name: props.profile.name
+            location: props.profile.location || '',
+            bio: props.profile.bio || '',
+            name: props.profile.name,
+            isSaved: false
         }
+        this.handleNameChange = this.handleNameChange.bind(this)
+        this.handleLocationChange = this.handleLocationChange.bind(this)
+        this.handleBioChange = this.handleBioChange.bind(this)
+        this.saveProfile = this.saveProfile.bind(this)
+    }
+
+    handleNameChange(e) {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    handleLocationChange(e) {
+        this.setState({
+            location: e.target.value
+        })
+    }
+
+    handleBioChange(e) {
+        this.setState({
+            bio: e.target.value
+        })
+    }
+
+    saveProfile() {
+        let that = this
+        axios.put(baseUrl + '/profile/' + this.props.profile._id, this.state, {
+            headers: {
+                Authorization: 'Bearer ' + this.props.token
+            }
+        }).then(res => {
+            that.props.handleUserUpdate({profile: res.data.profile})
+            that.setState({
+                isSaved: true
+            })
+        })
     }
 
     render() {
         return (
             <div className="col-2of5 bg-white profile">
-                <div class="relative img-edit">
-                    <img class="avatar" src={this.state.avatarUrl} alt="avatar" id="avatar-image" />
-                    <img class="avatar-upload" src="/img/upload.png" alt="upload-img" />
+                <div className="relative img-edit">
+                    <img className="avatar" src={this.state.avatarUrl} alt="avatar" id="avatar-image" />
+                    <img className="avatar-upload" src="/img/upload.png" alt="upload-img" />
                 </div>
-                <input class="input-profile" type="text" value={this.state.name} placeholder="Full name" />
+                <input className="input-profile" type="text" value={this.state.name} placeholder="Full name" onChange={this.handleNameChange} />
                 <h5 id="username">@{this.props.profile.username}</h5>
-                <input class="input-profile" type="text" value={this.state.location} placeholder="Location" />
-                <textarea class="input-profile" placeholder="Personal Bio">{this.state.bio}</textarea>
-                <button class="btn-primary space-top" type="button" id="save-btn">Save</button>
-                <button class="btn-border space-top" type="button" id="cancel-btn">Cancel</button>
+                <input className="input-profile" type="text" value={this.state.location} placeholder="Location" onChange={this.handleLocationChange} />
+                <textarea className="input-profile" placeholder="Personal Bio" onChange={this.handleBioChange} value={this.state.bio}></textarea>
+                {this.state.isSaved ? <Redirect to="/profile" /> : <button className="btn-primary space-top" type="button" onClick={this.saveProfile}>Save</button>}
+                <Link to="/profile"><button className="btn-border space-top" type="button">Cancel</button></Link>
             </div>
         );
     }
