c414098f1e12e21539d1d5fc5e325f77d67ff006
Yan Hong <yan@hackhub.com>
2020-02-16 14:12:47 -0800

    add rematch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { init } from '@rematch/core'
+import { Provider } from 'react-redux'
+import * as models from './models'
 
 // load components
 import Page from './component/Page';
 
+const store = init({
+  models
+})
+
 class App extends Component {
   render() {
     return (
-      <Router>  
+      <Provider store={store}>
+        <Router>
           <Page />
         </Router>
+      </Provider>
     );
   }
 }
---
+++ src/models.js
@@ -0,0 +1,21 @@
+export const user = {
+  state: {
+    profile: {}
+  },
+  reducers: {
+
+  },
+  effects: {
+
+  }
+}
+
+export const tweets = {
+  state: [],
+  reducers: {
+
+  },
+  effects: {
+    
+  }
+}
